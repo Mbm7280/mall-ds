4a24c6487d9ccfdf4b663213894b6b1c4f33ddfc
Echo <mbm1216@163.com>
2022-11-07 20:08:22 +0800

    mbg-生成mapper

---
+++ mall-ds/mall-mbg/src/main/java/com/echo/mallds/mapper/CmsTopicCategoryMapper.java
@@ -0,0 +1,30 @@
+package com.echo.mallds.mapper;
+
+import com.echo.mallds.model.CmsTopicCategory;
+import com.echo.mallds.model.CmsTopicCategoryExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CmsTopicCategoryMapper {
+    long countByExample(CmsTopicCategoryExample example);
+
+    int deleteByExample(CmsTopicCategoryExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(CmsTopicCategory row);
+
+    int insertSelective(CmsTopicCategory row);
+
+    List<CmsTopicCategory> selectByExample(CmsTopicCategoryExample example);
+
+    CmsTopicCategory selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") CmsTopicCategory row, @Param("example") CmsTopicCategoryExample example);
+
+    int updateByExample(@Param("row") CmsTopicCategory row, @Param("example") CmsTopicCategoryExample example);
+
+    int updateByPrimaryKeySelective(CmsTopicCategory row);
+
+    int updateByPrimaryKey(CmsTopicCategory row);
+}
